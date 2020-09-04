8bc7d122e9694e28ccd416e3d7a5aa29bfb1875c
Suizhazha <573505403@qq.com>
2020-09-04 15:13:48 +0800

    使用useReducer践行Flux/Redux思想

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
 import React from "react"
-import './index.css'
 
-const themeContext = React.createContext(null)
 
-const App = () => {
-const [theme,setTheme] = React.useState('red')
-  return (
-    <themeContext.Provider value={{theme,setTheme}}>
-      <div className={`App ${theme}`}>
-        <p>{theme}</p>
-        <div>
-          <Child1/>
-        </div>
-        <div>
-          <Child2/>
-        </div>
-      </div>
-    </themeContext.Provider>
+const initial = {
+  n:0
+}
+
+const reducer =(state,action) =>{
+  if (action.type=== 'add'){
+    return {
+      n:state.n+action.number
+    }
+  }else if (action.type==='multiply'){
+    return {
+      n:state.n*action.number
+    }
+  }
+  else {
+    throw new Error('unknown type')
+  }
+}
+
+const App=()=>{
+const [state,dispatch]=  React.useReducer(reducer,initial)
+
+
+const onClick = ()=>{
+  dispatch({type:'add',number:1})
+}
+const onClick2 = ()=>{
+  dispatch({type:'add',number:2})
+}
+  const onClick3 = ()=>{
+    dispatch({type:'multiply',number:2})
+  }
+  return(
+    <div>
+      <p>n:{state.n}</p>
+      <button onClick={onClick}>+1</button>
+      <button onClick={onClick2}>+2</button>
+
+      <button onClick={onClick3}>*2</button>
+
+    </div>
   )
-}
-
-const Child1 = () =>{
-  const {setTheme}  = React.useContext(themeContext)
-return(
-  <div>
-    <button onClick={()=>{setTheme('red')}}>red</button>
-  </div>
-)
-}
-const Child2 = () =>{
-  const {setTheme}  = React.useContext(themeContext)
-  return (
-  <div>
-    <button onClick={()=>{setTheme('blue')}}>blue</button>
-  </div>
-)
 }
 
 
